aa8f4dd9b2b0b55ecac4e10dceeb9eb1c629425b
Welen1911 <welenalmeida2210@gmail.com>
2024-03-15 16:34:54 -0300

    Primeiro commit

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +9,18 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
-    <a class="text-danger" href="{{ route('pdf.download') }}">DownloadPDF</a>
+    <div>
+        <h1 style="text-align: center">
+            Olá, {{$user->name}}!
+        </h1>
+    </div>
+    <ul>
+        <li style="color: red">{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+        <li>{{ $user->id }}</li>
+    </ul>
 </body>
+
 </html>
